292149fdf165f963fff4e580bcd5de0e2dbf2d5f
Bogdan Grec <bogdangrec@icloud.com>
2020-10-11 19:09:38 +0200

    ios 14

---
+++ Shared/eSTEstudioApp_.swift
@@ -0,0 +1,21 @@
+//
+//  eSTEstudioApp.swift
+//  Shared
+//
+//  Created by Bogdan Grec on 11/10/20.
+//
+/*
+import SwiftUI
+
+@main
+struct eSTEstudioApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
+*/
